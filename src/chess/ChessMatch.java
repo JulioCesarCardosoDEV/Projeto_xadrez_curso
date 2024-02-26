024de3a1e2d5ed00c29e42c7eecb070c50c815ab
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-02-26 20:55:43 -0300

    Criando turnos e a vez de cada jogador jogar

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
+	private int turn;
+	private Color currentPlayer;
 	
 	// Definindo o tamanho do tabuleiro
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -44,6 +56,8 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		// Troca de jogada
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -51,6 +65,11 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe uma peça nessa origem");
+		}
+		
+		// Método que verifica se a peça pertence ao jogador
+		if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("A peça escolhida não é sua");
 		}
 		
 		// Método que trata a exceção = movimento escolhido não existe
@@ -76,6 +95,15 @@ public class ChessMatch {
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
+	}
+	
+	// Método que realiza a troca de turnos
+	private void nextTurn() {
+		// Próximo turno
+		turn ++;
+		// Se o jogador atual = white, ele agora vai passar a vez para a cor black
+		// Se o jogador atual = black, ele agora vai passar a vez para a cor white
+		currentPlayer = (currentPlayer == Color.WHITE)? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
