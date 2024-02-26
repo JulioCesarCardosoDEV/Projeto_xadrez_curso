d05cc11340cc948a4409689258e86f8080afdae1
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-02-26 11:18:28 -0300

    Implementando movimentos possíveis do rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,23 +1,83 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
 	}
 
+	private boolean canMove(Position posision) {
+		ChessPiece p = (ChessPiece) getBoard().piece(posision);
+
+		// Retonar p se ele for deferente de nulo, ou for uma peça adversária
+		return p == null || p.getColor() != getColor();
+
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// Realizando movimento para cima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Realizando movimento para diagonal noroeste
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para diagonal nordeste
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para diagonal sudoeste
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Realizando movimento para diagonal sudeste
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+				
 		return matriz;
 	}
 
